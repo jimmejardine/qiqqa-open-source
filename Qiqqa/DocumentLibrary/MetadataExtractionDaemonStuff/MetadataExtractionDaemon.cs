8a1d7660659079939e59be74bf3822ea6311a205
Ger Hobbelt <ger@hobbelt.com>
2019-08-02 13:34:24 +0200

    Fix https://github.com/jimmejardine/qiqqa-open-source/issues/17 by processing PDFs in any Qiqqa library in *small* batches so that Qiqqa is not unreponsive for a loooooooooooooong time when it is re-indexing/upgrading/whatever a *large* library, e.g. 20K+ PDF files. The key here is to make the '**infrequent background task**' produce *some* result quickly (like a working, yet incomplete, Lucene search index DB!) and then *updating*/*augmenting* that result as time goes by. This way, we can recover a search index for larger Qiqqa libraries!

--- Qiqqa/DocumentLibrary/MetadataExtractionDaemonStuff/MetadataExtractionDaemon.cs
+++ Qiqqa/DocumentLibrary/MetadataExtractionDaemonStuff/MetadataExtractionDaemon.cs
@@ -18,114 +18,175 @@ namespace Qiqqa.DocumentLibrary.MetadataExtractionDaemonStuff
         public void DoMaintenance(Library library, Daemon daemon)
         {
             Stopwatch sw_total = new Stopwatch();
-            sw_total.Start();            
+            sw_total.Start();
 
-            // Check that we have something to do
-            List<PDFDocument> pdfs_to_process = new List<PDFDocument>();
+            Logging.Debug("MetadataExtractionDaemon::DoMaintenance START");
+
+            // To recover from a search index fatal failure and re-indexing attempt for very large libraries,
+            // we're better off processing a limited number of source files as we'll be able to see 
+            // *some* results more quickly and we'll have a working, though yet incomplete,
+            // index in *reasonable time*.
+            //
+            // To reconstruct the entire index will take a *long* time. We grow the index and other meta
+            // stores a bunch-of-files at a time and then repeat the entire maintenance process until
+            // we'll be sure to have run out of files to process for sure...
+            const int MAX_NUMBER_OF_PDF_FILES_TO_PROCESS = 10;
+            const int MAX_SECONDS_PER_ITERATION = 15;
+            DateTime index_processing_start_time = DateTime.UtcNow;
+
+            while (true)
             {
-                List<PDFDocument> pdf_documents = library.PDFDocuments;
-                foreach (PDFDocument pdf_document in pdf_documents)
+                // If this library is busy, skip it for now
+                if (Library.IsBusyAddingPDFs)
                 {
-                    bool needs_processing = false;
-                    if (PDFMetadataInferenceFromPDFMetadata.NeedsProcessing(pdf_document)) needs_processing = true;
-                    if (PDFMetadataInferenceFromOCR.NeedsProcessing(pdf_document)) needs_processing = true;
-                    if (PDFMetadataInferenceFromBibTeXSearch.NeedsProcessing(pdf_document)) needs_processing = true;
-                        
-                    if (needs_processing)
+                    Logging.Info("MetadataExtractionDaemon::DoMaintenance: Not daemon processing a library that is busy with adds...");
+                    break;
+                }
+
+                if (DateTime.UtcNow.Subtract(index_processing_start_time).TotalSeconds > MAX_SECONDS_PER_ITERATION)
+                {
+                    Logging.Info("DoMaintenance: Breaking out of outer processing loop due to MAX_SECONDS_PER_ITERATION: {0} seconds consumed", DateTime.UtcNow.Subtract(index_processing_start_time).TotalSeconds);
+                    break;
+                }
+
+                if (!daemon.StillRunning)
+                {
+                    Logging.Debug("DoMaintenance: Breaking out of outer processing loop due to daemon termination");
+                    break;
+                }
+
+                // Check that we have something to do
+                List<PDFDocument> pdfs_to_process = new List<PDFDocument>();
+                {
+                    List<PDFDocument> pdf_documents = library.PDFDocuments;
+                    foreach (PDFDocument pdf_document in pdf_documents)
                     {
-                        pdfs_retry_count.TallyOne(pdf_document.Fingerprint);
-                        if (General.IsPowerOfTwo(pdfs_retry_count.GetCount(pdf_document.Fingerprint)))
+                        bool needs_processing = false;
+                        if (PDFMetadataInferenceFromPDFMetadata.NeedsProcessing(pdf_document)) needs_processing = true;
+                        if (PDFMetadataInferenceFromOCR.NeedsProcessing(pdf_document)) needs_processing = true;
+                        if (PDFMetadataInferenceFromBibTeXSearch.NeedsProcessing(pdf_document)) needs_processing = true;
+
+                        if (needs_processing)
                         {
-                            pdfs_to_process.Add(pdf_document);
+                            pdfs_retry_count.TallyOne(pdf_document.Fingerprint);
+                            if (General.IsPowerOfTwo(pdfs_retry_count.GetCount(pdf_document.Fingerprint)))
+                            {
+                                pdfs_to_process.Add(pdf_document);
+                            }
                         }
+
+                        if (!daemon.StillRunning)
+                        {
+                            Logging.Debug("Breaking out of MetadataExtractionDaemon PDF fingerprinting loop due to daemon termination");
+                            break;
+                        }
+
+                        // Limit the number of source files to process at once or we won't have recreated
+                        // a sane (though tiny and incomplete) Lucene search index database by the time 
+                        // the user exists the Qiqqa application in a minute or so.
+                        // When the user keeps Qiqqa running, this same approach will help us to 'update'
+                        // the search index a bunch of files at a time, so everyone involved will be able
+                        // to see progress happening after losing the index due to some fatal crash or
+                        // forced re-index request.
+                        if (pdfs_to_process.Count >= MAX_NUMBER_OF_PDF_FILES_TO_PROCESS)
+                        {
+                            Logging.Debug("Breaking out of MetadataExtractionDaemon PDF fingerprinting loop due to MAX_NUMBER_OF_PDF_FILES_TO_PROCESS reached");
+                            break;
+                        }
+                    }
+
+                    if (0 < pdfs_to_process.Count)
+                    {
+                        Logging.Debug("Got {0} items of metadata extraction work", pdfs_to_process.Count);
+                    }
+                }
+
+                // Get each of our guys to start rendering their first pages so we can do some extraction
+                foreach (PDFDocument pdf_document in pdfs_to_process)
+                {
+                    if (!daemon.StillRunning)
+                    {
+                        Logging.Debug("Breaking out of MetadataExtractionDaemon PDF processing loop due to daemon termination");
+                        break;
+                    }
+
+                    try
+                    {
+                        if (pdf_document.DocumentExists)
+                        {
+                            pdf_document.PDFRenderer.GetOCRText(1);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Logging.Error(ex, "There was an exception while requesting the first page to be OCRed");
+                    }
+                }
+
+                // See if there is any completed OCR to work with
+                if (0 < pdfs_to_process.Count)
+                {
+                    StatusManager.Instance.ClearCancelled("AutoSuggestMetadata");
+                }
+
+                for (int i = 0; i < pdfs_to_process.Count; ++i)
+                {
+                    StatusManager.Instance.UpdateStatusBusy("AutoSuggestMetadata", "Suggesting metadata", i, pdfs_to_process.Count, true);
+                    if (StatusManager.Instance.IsCancelled("AutoSuggestMetadata"))
+                    {
+                        break;
+                    }
+
+                    if (!daemon.StillRunning)
+                    {
+                        Logging.Debug("Breaking out of MetadataExtractionDaemon metadata suggesting loop due to daemon termination");
+                        break;
+                    }
+
+                    PDFDocument pdf_document = pdfs_to_process[i];
+
+                    // Try get the authors and year with the PDF in-file metadata
+                    try
+                    {
+                        PDFMetadataInferenceFromPDFMetadata.InferFromPDFMetadata(pdf_document);
+                    }
+                    catch (Exception ex)
+                    {
+                        Logging.Warn(ex, "Problem in PDFMetadataInferenceFromPDFMetadata.InferFromPDFMetadata while processing document {0}", pdf_document.Fingerprint);
+                    }
+
+                    // Try looking for the title in the OCR
+                    try
+                    {
+                        PDFMetadataInferenceFromOCR.InferTitleFromOCR(pdf_document);
+                    }
+                    catch (Exception ex)
+                    {
+                        Logging.Warn(ex, "Problem in PDFMetadataInferenceFromOCR.InferTitleFromOCR while processing document {0}", pdf_document.Fingerprint);
+                    }
+
+                    // Try suggesting some bibtex from bibtexsearch.com
+                    try
+                    {
+                        PDFMetadataInferenceFromBibTeXSearch.InferBibTeX(pdf_document, false);
+                    }
+                    catch (Exception ex)
+                    {
+                        Logging.Warn(ex, "Problem in PDFMetadataInferenceFromOCR.InferTitleFromOCR while processing document {0}", pdf_document.Fingerprint);
                     }
                 }
 
                 if (0 < pdfs_to_process.Count)
                 {
-                    Logging.Debug("Got {0} items of metadata extraction work", pdfs_to_process.Count);
+                    Logging.Info("It took a total of {0}ms to extract metadata", sw_total.ElapsedMilliseconds);
+                    StatusManager.Instance.ClearStatus("AutoSuggestMetadata");
                 }
-            }
-
-            // Get each of our guys to start rendering their first pages so we can do some extraction
-            foreach (PDFDocument pdf_document in pdfs_to_process)
-            {
-                if (!daemon.StillRunning)
+                else
                 {
+                    Logging.Debug("DoMaintenance: Breaking out of outer processing loop due to no more files to process (count = {0})", pdfs_to_process.Count);
                     break;
                 }
-
-                try
-                {
-                    if (pdf_document.DocumentExists)
-                    {
-                        pdf_document.PDFRenderer.GetOCRText(1);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Logging.Error(ex, "There was an exception while requesting the first page to be OCRed");
-                }
-            }
-
-            // See if there is any completed OCR to work with
-            if (0 < pdfs_to_process.Count)
-            {
-                StatusManager.Instance.ClearCancelled("AutoSuggestMetadata");
-            }
-
-            for (int i = 0; i < pdfs_to_process.Count; ++i)
-            {
-                StatusManager.Instance.UpdateStatusBusy("AutoSuggestMetadata", "Suggesting metadata", i, pdfs_to_process.Count, true);
-                if (StatusManager.Instance.IsCancelled("AutoSuggestMetadata"))
-                {
-                    break;
-                }
-
-                if (!daemon.StillRunning)
-                {
-                    break;
-                }
-
-                PDFDocument pdf_document = pdfs_to_process[i];
-
-                // Try get the authors and year with the PDF in-file metadata
-                try
-                {
-                    PDFMetadataInferenceFromPDFMetadata.InferFromPDFMetadata(pdf_document);
-                }
-                catch (Exception ex)
-                {
-                    Logging.Warn(ex, "Problem in PDFMetadataInferenceFromPDFMetadata.InferFromPDFMetadata while processing document {0}", pdf_document.Fingerprint);
-                }
-
-                // Try looking for the title in the OCR
-                try
-                {
-                    PDFMetadataInferenceFromOCR.InferTitleFromOCR(pdf_document);
-                }
-                catch (Exception ex)
-                {
-                    Logging.Warn(ex, "Problem in PDFMetadataInferenceFromOCR.InferTitleFromOCR while processing document {0}", pdf_document.Fingerprint);
-                }
-
-                // Try suggesting some bibtex from bibtexsearch.com
-                try
-                {
-                    PDFMetadataInferenceFromBibTeXSearch.InferBibTeX(pdf_document, false);
-                }
-                catch (Exception ex)
-                {
-                    Logging.Warn(ex, "Problem in PDFMetadataInferenceFromOCR.InferTitleFromOCR while processing document {0}", pdf_document.Fingerprint);
-                }
-            }
-
-            if (0 < pdfs_to_process.Count)
-            {
-                Logging.Info("It took a total of {0}ms to extract metadata", sw_total.ElapsedMilliseconds);
-                StatusManager.Instance.ClearStatus("AutoSuggestMetadata");
             }
         }
-
     }
 }
