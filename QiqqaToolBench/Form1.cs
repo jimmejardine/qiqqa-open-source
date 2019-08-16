b0b7e7224ed1e94731d126424c08a5e95a19eba0
Ger Hobbelt <ger@hobbelt.com>
2019-08-16 14:20:36 +0200

    - 'integrate' nant build script for producing setup.exe installers into MSVS2019 solution by using a dummy project and prebuild script - added skeleton projects for qiqqa diagnostic and migration work. Related to https://github.com/jimmejardine/qiqqa-open-source/issues/43

---
+++ QiqqaToolBench/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QiqqaToolBench
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckLibraryIntegrityToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SelectLibraryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewPDFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewWebPageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GrabSomeBibTexOffGoogleScholarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewPDFAndMarkATitleTextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
