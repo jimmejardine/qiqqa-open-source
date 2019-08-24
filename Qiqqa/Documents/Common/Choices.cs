7065070697e10e8c234a357b94f9dedee0da0e23
Ger Hobbelt <ger@hobbelt.com>
2019-08-24 11:13:37 +0200

    code cleanup: encode all reading stages

--- Qiqqa/Documents/Common/Choices.cs
+++ Qiqqa/Documents/Common/Choices.cs
@@ -9,11 +9,16 @@ namespace Qiqqa.Documents.Common
             new string[] { "", "1", "2", "3", "4", "5" }
         );
 
-        public static readonly string ReadingStages_INTERRUPTED = "Interrupted";
-        public static readonly string ReadingStages_TOP_PRIORITY = "Top priority";
-        public static readonly string ReadingStages_READ_AGAIN = "Read again";
-        public static readonly string ReadingStages_UNREAD = "Unread";
+        public const string ReadingStages_INTERRUPTED = "Interrupted";
+        public const string ReadingStages_TOP_PRIORITY = "Top priority";
+        public const string ReadingStages_READ_AGAIN = "Read again";
+        public const string ReadingStages_UNREAD = "Unread";
         public const string ReadingStages_DUPLICATE = "Duplicate";
+        public const string ReadingStages_STARTED_READING = "Started reading";
+        public const string ReadingStages_FINISHED_READING = "Finished reading";
+        public const string ReadingStages_BROWSED = "Browsed";
+        public const string ReadingStages_SKIM_READ = "Skim read";
+        public const string ReadingStages_INTEREST_ONLY = "Interest only";
         
         public static readonly List<string> ReadingStages = new List<string>
         (
@@ -21,14 +26,14 @@ namespace Qiqqa.Documents.Common
             { 
                 "",
                 ReadingStages_TOP_PRIORITY,
-                "Started reading",                
+                ReadingStages_STARTED_READING,                
                 ReadingStages_INTERRUPTED,
                 ReadingStages_UNREAD,
                 ReadingStages_READ_AGAIN,
-                "Finished reading",
-                "Browsed",
-                "Skim read",
-                "Interest only",
+                ReadingStages_FINISHED_READING,
+                ReadingStages_BROWSED,
+                ReadingStages_SKIM_READ,
+                ReadingStages_INTEREST_ONLY,
                 ReadingStages_DUPLICATE
             }
         );
