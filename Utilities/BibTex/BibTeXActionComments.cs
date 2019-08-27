f8d5ff7c9ba1c2d5a65501c81d9d12609cefc98e
Ger Hobbelt <ger@hobbelt.com>
2019-08-27 18:50:10 +0200

    BibTeX modes for manual editing and vetting in the Sniffer.

--- Utilities/BibTex/BibTeXActionComments.cs
+++ Utilities/BibTex/BibTeXActionComments.cs
@@ -10,5 +10,7 @@ namespace Utilities.BibTex
         public static readonly string SKIP = "@comment { BIBTEX_SKIP }";
         public static readonly string AUTO_BIBTEXSEARCH = "@comment { BIBTEX_AUTO - BIBTEXSEARCH }";
         public static readonly string AUTO_GS = "@comment { BIBTEX_AUTO - GS }";
+        public static readonly string USER_VETTED = "@comment { BIBTEX_USER_VETTED }";
+        public static readonly string MANUAL_EDIT = "@comment { BIBTEX_MANUAL_EDIT }";
     }
 }
