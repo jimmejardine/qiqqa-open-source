68b39f5ac0548aa403492960d02b49ca61108988
Ger Hobbelt <ger@hobbelt.com>
2019-09-06 04:40:08 +0200

    moved the `\n\n\n` append patch to the BibTeX lexer and started the refactor there. Working towards https://github.com/jimmejardine/qiqqa-open-source/issues/68

--- Utilities/BibTex/Parsing/BibTexAssembler.cs
+++ Utilities/BibTex/Parsing/BibTexAssembler.cs
@@ -7,10 +7,7 @@ namespace Utilities.BibTex.Parsing
         internal static BibTexParseResult Parse(string bibtex)
         {
             BibTexAssembler assembler = new BibTexAssembler();
-            // append some harmless whitespace at the end to help 
-            // reduce the number of out-of-bounds exceptions in the parser/lexer
-            // while keeping the code simple:
-            BibTexLexer lexer = new BibTexLexer(bibtex + "\n\n\n");
+            BibTexLexer lexer = new BibTexLexer(bibtex);
             lexer.Parse(assembler);
             return new BibTexParseResult(assembler.items, assembler.comments);
         }
