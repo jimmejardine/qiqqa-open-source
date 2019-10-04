623ad00621dd363f5fcf6e1ab09ed2c12f9369b7
Ger Hobbelt <ger@hobbelt.com>
2019-10-04 16:16:37 +0200

    working towards https://github.com/jimmejardine/qiqqa-open-source/issues/43 and offloading the old cruft to a dedicated library so the mainline codebase doesn't keep cluttered with old stuff, just because we want to be able to load/import old Qiqqa libraries.

---
+++ QiqqaLegacyFileFormats/ObjectSerializer.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Utilities.Reflection
+{
+    public class ObjectSerializer
+    {
+        public static object LoadObject(string filename)
+        {
+            using (FileStream fs = new FileStream(filename, FileMode.Open))
+            {
+                return LoadObject(fs);
+            }
+        }
+
+        public static object LoadObject(Stream stream)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            return bf.Deserialize(stream);
+        }
+
+        public static void SaveObject(string filename, object obj)
+        {
+            // Make sure the path exists
+            string pathname = Path.GetDirectoryName(filename);
+            Directory.CreateDirectory(pathname);
+
+            using (FileStream fs = File.OpenWrite(filename))
+            {
+                SaveObject(fs, obj);
+            }
+        }
+
+        public static void SaveObject(Stream stream, object obj)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(stream, obj);
+        }
+    }
+}
