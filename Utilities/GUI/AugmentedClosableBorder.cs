ace54582f7022884d70e1ef0b86b2515afbed5b4
Ger Hobbelt <ger@hobbelt.com>
2019-10-21 23:53:45 +0200

    prepwork for https://github.com/jimmejardine/qiqqa-open-source/issues/119: closables should have a name so we can create an identification path for them by traversing the WPF/UI tree: every such path can be checkec and hidden/shown individually, even persisted across runs.

--- Utilities/GUI/AugmentedClosableBorder.cs
+++ Utilities/GUI/AugmentedClosableBorder.cs
@@ -19,6 +19,32 @@ namespace Utilities.GUI
         {
             Theme.Initialize();
 
+            this.Unloaded += AugmentedClosableBorder_Unloaded;
+
+            //
+            // Summary:
+            //     Occurs when either the System.Windows.FrameworkElement.ActualHeight or the System.Windows.FrameworkElement.ActualWidth
+            //     properties change value on this element.
+            this.SizeChanged += AugmentedClosableBorder_SizeChanged;
+
+            //
+            // Summary:
+            //     Occurs when System.Windows.FrameworkElement.BringIntoView(System.Windows.Rect)
+            //     is called on this element.
+            this.RequestBringIntoView += AugmentedClosableBorder_RequestBringIntoView;
+
+            //
+            // Summary:
+            //     Occurs when the element is laid out, rendered, and ready for interaction.
+            this.Loaded += AugmentedClosableBorder_Loaded;
+
+            //
+            // Summary:
+            //     Occurs when this System.Windows.FrameworkElement is initialized. This event coincides
+            //     with cases where the value of the System.Windows.FrameworkElement.IsInitialized
+            //     property changes from false (or undefined) to true.
+            this.Initialized += AugmentedClosableBorder_Initialized;
+
             this.CornerRadius = new CornerRadius(3);
             this.BorderBrush = ThemeColours.Background_Brush_Blue_VeryDark;
             this.BorderThickness = new Thickness(1);
@@ -43,18 +69,25 @@ namespace Utilities.GUI
 
             this.GridBackground.Children.Add(image_close);
 
-            this.Child = GridBackground;            
+            this.Child = GridBackground;
         }
 
         void AugmentedClosableBorder_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-            if (has_been_force_closed_by_user) this.Visibility = Visibility.Collapsed;
+            if (has_been_force_closed_by_user)
+            {
+                IdPath = GetIdPath();
+                this.Visibility = Visibility.Collapsed;
+            }
+            IdPath = GetIdPath();
         }
 
         void image_close_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             this.has_been_force_closed_by_user = true;
             this.Visibility = Visibility.Collapsed;
+            IdPath = GetIdPath();
+
         }
 
         [Bindable(true)]
@@ -78,5 +111,108 @@ namespace Utilities.GUI
                 }
             }
         }
+
+        // -----------------------------------------------------------------------------
+
+        //
+        // Summary:
+        //     When overridden in a derived class, returns an alternative user interface (UI)
+        //     parent for this element if no visual parent exists.
+        //
+        // Returns:
+        //     An object, if implementation of a derived class has an alternate parent connection
+        //     to report.
+        protected override DependencyObject GetUIParentCore()
+        {
+            return base.GetUIParentCore();
+        }
+
+        //
+        // Summary:
+        //     Called when the parent of the visual object is changed.
+        //
+        // Parameters:
+        //   oldParent:
+        //     A value of type System.Windows.DependencyObject that represents the previous
+        //     parent of the System.Windows.Media.Visual object. If the System.Windows.Media.Visual
+        //     object did not have a previous parent, the value of the parameter is null.
+        protected override void OnVisualParentChanged(DependencyObject oldParent)
+        {
+            base.OnVisualParentChanged(oldParent);
+        }
+
+    private void AugmentedClosableBorder_Unloaded(object sender, RoutedEventArgs e)
+    {
+            Logging.Debug("not implemented");
+        }
+
+        private void AugmentedClosableBorder_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+            Logging.Debug("not implemented");
+            IdPath = GetIdPath();
+        }
+
+        private void AugmentedClosableBorder_Initialized(object sender, System.EventArgs e)
+        {
+            Logging.Debug("not implemented");
+        }
+
+        string IdPath;
+
+        private void AugmentedClosableBorder_Loaded(object sender, RoutedEventArgs e)
+        {
+            Logging.Debug("not implemented");
+
+            IdPath = GetIdPath();
+        }
+
+        private void AugmentedClosableBorder_RequestBringIntoView(object sender, RequestBringIntoViewEventArgs e)
+        {
+            Logging.Debug("not implemented");
+        }
+
+
+        public string GetIdPath()
+        {
+            if (string.IsNullOrEmpty(this.Name))
+            {
+                return null;
+            }
+            if (null == this.Parent)
+            {
+                return null;
+            }
+            string rv = this.Name;
+            var p = this.Parent;
+            while (null != p)
+            {
+                FrameworkElement el = p as FrameworkElement;
+                if (null != el)
+                {
+                    if (!string.IsNullOrEmpty(el.Name))
+                    {
+                        rv = el.Name + "/" + rv;
+                    }
+                    p = el.Parent;
+                }
+                else
+                {
+                    p = null;
+                }
+            }
+
+            return rv;
+        }
+
     }
 }
+
+
+
+
+
+
+
+//
+// Summary:
+//     Occurs when the element is removed from within an element tree of loaded elements.
