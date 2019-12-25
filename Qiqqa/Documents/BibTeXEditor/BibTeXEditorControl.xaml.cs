60392ba93a861830031a28b6d529461c6ec24f06
Ger Hobbelt <ger@hobbelt.com>
2019-12-25 01:21:55 +0100

    fix crash: 91224235436:System.NullReferenceException: stacktrace:
    - Object reference not set to an instance of an object
      - at Qiqqa.Documents.BibTeXEditor.BibTeXEditorControl.RegisterOverlayButtons(FrameworkElement BibTeXParseErrorButton, FrameworkElement BibTeXModeToggleButton, FrameworkElement BibTeXUndoEditButton, Double IconHeight) in \Qiqqa\Documents\BibTeXEditor\BibTeXEditorControl.xaml.cs:line 172
      - at Qiqqa.Documents.PDF.PDFControls.MetadataControls.MetadataBibTeXEditorControl.OnClosed(EventArgs e) in \Qiqqa\Documents\PDF\PDFControls\MetadataControls\MetadataBibTeXEditorControl.xaml.cs:line 122
      - at System.Windows.Window.WmDestroy()

--- Qiqqa/Documents/BibTeXEditor/BibTeXEditorControl.xaml.cs
+++ Qiqqa/Documents/BibTeXEditor/BibTeXEditorControl.xaml.cs
@@ -159,69 +159,79 @@ namespace Qiqqa.Documents.BibTeXEditor
             BibTeXModeToggleButtonRef = new WeakReference<FrameworkElement>(BibTeXModeToggleButton);
             BibTeXUndoEditButtonRef = new WeakReference<FrameworkElement>(BibTeXUndoEditButton);
 
-            if (BibTeXParseErrorButton is AugmentedButton)
+            // button references MAY be NULL when this method is invoked on dialog close as part of the cleanup:
+            if (BibTeXParseErrorButton != null)
             {
-                AugmentedButton btn = BibTeXParseErrorButton as AugmentedButton;
-                btn.Caption = "View parse errors";
-                btn.Icon = Icons.GetAppIcon(Icons.BibTeXParseError2);
-                if (IconHeight > 0)
+                if (BibTeXParseErrorButton is AugmentedButton)
                 {
-                    btn.IconHeight = IconHeight;
+                    AugmentedButton btn = BibTeXParseErrorButton as AugmentedButton;
+                    btn.Caption = "View parse errors";
+                    btn.Icon = Icons.GetAppIcon(Icons.BibTeXParseError2);
+                    if (IconHeight > 0)
+                    {
+                        btn.IconHeight = IconHeight;
+                    }
                 }
-            }
-            BibTeXParseErrorButton.Visibility = Visibility.Visible;
-            BibTeXParseErrorButton.IsEnabled = false;
-            BibTeXParseErrorButton.Opacity = 0.3;
-            BibTeXParseErrorButton.ToolTip = "Shows BibTeX errors when they occur";
-            if (BibTeXParseErrorButton is Image)
-            {
-                Image imgBtn = BibTeXParseErrorButton as Image;
-                imgBtn.Source = Icons.GetAppIcon(Icons.BibTeXParseError2);
-                RenderOptions.SetBitmapScalingMode(imgBtn, BitmapScalingMode.HighQuality);
+                BibTeXParseErrorButton.Visibility = Visibility.Visible;
+                BibTeXParseErrorButton.IsEnabled = false;
+                BibTeXParseErrorButton.Opacity = 0.3;
+                BibTeXParseErrorButton.ToolTip = "Shows BibTeX errors when they occur";
+                if (BibTeXParseErrorButton is Image)
+                {
+                    Image imgBtn = BibTeXParseErrorButton as Image;
+                    imgBtn.Source = Icons.GetAppIcon(Icons.BibTeXParseError2);
+                    RenderOptions.SetBitmapScalingMode(imgBtn, BitmapScalingMode.HighQuality);
+                }
             }
 
-            if (BibTeXModeToggleButton is AugmentedButton)
+            if (BibTeXModeToggleButton != null)
             {
-                AugmentedButton btn = BibTeXModeToggleButton as AugmentedButton;
-                btn.Caption = "Toggle edit mode";
-                btn.Icon = Icons.GetAppIcon(Icons.BibTeXEditToggleMode1);
-                if (IconHeight > 0)
+                if (BibTeXModeToggleButton is AugmentedButton)
                 {
-                    btn.IconHeight = IconHeight;
+                    AugmentedButton btn = BibTeXModeToggleButton as AugmentedButton;
+                    btn.Caption = "Toggle edit mode";
+                    btn.Icon = Icons.GetAppIcon(Icons.BibTeXEditToggleMode1);
+                    if (IconHeight > 0)
+                    {
+                        btn.IconHeight = IconHeight;
+                    }
                 }
-            }
-            BibTeXModeToggleButton.Visibility = Visibility.Visible;
-            BibTeXModeToggleButton.ToolTip = "Toggle between a BibTeX grid ('parsed mode') and raw BibTeX text ('raw/unparssed mode').";
-            if (BibTeXModeToggleButton is Image)
-            {
-                Image imgBtn = BibTeXModeToggleButton as Image;
-                imgBtn.Source = Icons.GetAppIcon(Icons.BibTeXEditToggleMode1);
-                RenderOptions.SetBitmapScalingMode(imgBtn, BitmapScalingMode.HighQuality);
-            }
-            BibTeXModeToggleButton.Cursor = Cursors.Hand;
+                BibTeXModeToggleButton.Visibility = Visibility.Visible;
+                BibTeXModeToggleButton.ToolTip = "Toggle between a BibTeX grid ('parsed mode') and raw BibTeX text ('raw/unparssed mode').";
+                if (BibTeXModeToggleButton is Image)
+                {
+                    Image imgBtn = BibTeXModeToggleButton as Image;
+                    imgBtn.Source = Icons.GetAppIcon(Icons.BibTeXEditToggleMode1);
+                    RenderOptions.SetBitmapScalingMode(imgBtn, BitmapScalingMode.HighQuality);
+                }
+                BibTeXModeToggleButton.Cursor = Cursors.Hand;
 #if false
-            BibTeXModeToggleButton.MouseDown += ToggleBibTeXMode;
+                BibTeXModeToggleButton.MouseDown += ToggleBibTeXMode;
 #endif
-
-            if (BibTeXUndoEditButton is AugmentedButton)
-            {
-                AugmentedButton btn = BibTeXUndoEditButton as AugmentedButton;
-                btn.Caption = "Undo";
-                btn.Icon = Icons.GetAppIcon(Icons.Previous2);
-                if (IconHeight > 0)
-                {
-                    btn.IconHeight = IconHeight;
-                }
             }
-            BibTeXModeToggleButton.Visibility = Visibility.Visible;
-            BibTeXModeToggleButton.ToolTip = null;
-            BibTeXUndoEditButton.IsEnabled = false;
-            BibTeXUndoEditButton.Opacity = 0.3;
-            if (BibTeXUndoEditButton is Image)
+
+            if (BibTeXUndoEditButton != null)
             {
-                Image imgBtn = BibTeXUndoEditButton as Image;
-                imgBtn.Source = Icons.GetAppIcon(Icons.Previous2);
-                RenderOptions.SetBitmapScalingMode(imgBtn, BitmapScalingMode.HighQuality);
+                if (BibTeXUndoEditButton is AugmentedButton)
+                {
+                    AugmentedButton btn = BibTeXUndoEditButton as AugmentedButton;
+                    btn.Caption = "Undo";
+                    btn.Icon = Icons.GetAppIcon(Icons.Previous2);
+                    if (IconHeight > 0)
+                    {
+                        btn.IconHeight = IconHeight;
+                    }
+                }
+                BibTeXModeToggleButton.Visibility = Visibility.Visible;
+                BibTeXModeToggleButton.ToolTip = null;
+                BibTeXUndoEditButton.IsEnabled = false;
+                BibTeXUndoEditButton.Opacity = 0.3;
+                if (BibTeXUndoEditButton is Image)
+                {
+                    Image imgBtn = BibTeXUndoEditButton as Image;
+                    imgBtn.Source = Icons.GetAppIcon(Icons.Previous2);
+                    RenderOptions.SetBitmapScalingMode(imgBtn, BitmapScalingMode.HighQuality);
+                }
             }
         }
 
