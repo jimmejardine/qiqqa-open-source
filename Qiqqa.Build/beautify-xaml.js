1f1d5af5f885812257edd1972337be1fb58059d5
Ger Hobbelt <ger@hobbelt.com>
2019-12-30 19:25:22 +0100

    adjusted the npm package used for XML beautification

--- Qiqqa.Build/beautify-xaml.js
+++ Qiqqa.Build/beautify-xaml.js
@@ -1,16 +1,13 @@
 
-var beautify = require('xml-beautifier');
+var beautify = require('@gerhobbelt/xml-beautifier');
 var fs = require('fs');
 var path = require('path');
 
-const xml = beautify('<div><span>foo</span></div>');
-console.log(xml); // => will output correctly indented elements
 
 var xaml_list = [].concat(process.argv).slice(2);
-console.log(xaml_list);
 
-foreach(var source_file in xaml_list) {
-	console.log(`processing file ${source_file}:");
+for(const source_file of xaml_list) {
+	console.log(`processing file ${source_file}:`);
 	var src = fs.readFileSync(source_file, 'utf8');
 	var dst = beautify(src);
 	fs.writeFileSync(source_file, dst, 'utf8');
