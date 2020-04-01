b9ee51849ebb560508711b0ea2d6517ef47cf81d
Ger Hobbelt <ger@hobbelt.com>
2020-04-01 18:16:01 +0200

    introducing `vuepress` as the documentation gh-pages generating tool

---
+++ docs-src/.vuepress/config.js
@@ -0,0 +1,13 @@
+// https://vuepress.vuejs.org/guide/basic-config.html#config-file
+// https://vuepress.vuejs.org/config/#basic-config
+
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+
+  base: '/qiqqa/',
+  port: 8043,			// port # on the dev server
+  dest: 'docs/',
+
+  evergreen: true,
+};
