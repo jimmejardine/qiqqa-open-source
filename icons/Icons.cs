04d1931177325d895f5bacfbaff3372daa36744a
mahfiaz <mahfiaz@gmail.com>
2020-04-27 17:32:50 +0300

    Reverted removal of icon needed for alternative.to nagger.
    # Conflicts:
    #	icons/icons.csproj

--- icons/Icons.cs
+++ icons/Icons.cs
@@ -14,9 +14,9 @@ namespace icons
         public const string QiqqaAndroid = "QiqqaAndroid";
 
         public const string AugmentedButtonDown = "AugmentedButtonDown";
-        public const string Champion = "Champion";
         public const string ToolbarBackground = "ToolbarBackground";
 
+        public const string Champion = "Champion";
         public const string Plus = "Plus";
         public const string Minus = "Minus";
         public const string Yes = "Yes";
