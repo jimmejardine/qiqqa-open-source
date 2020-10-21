0c67136ea9050d90bb0958aa73ecdf0bf722f2fd
Ger Hobbelt <ger@hobbelt.com>
2020-10-21 16:17:18 +0200

    updated version

--- Utilities/AssemblyInfo.cs
+++ Utilities/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("82.0.7555.31312")]
-[assembly: AssemblyFileVersion("82.0.7555.31312")]
+[assembly: AssemblyVersion("82.0.7568.29227")]
+[assembly: AssemblyFileVersion("82.0.7568.29227")]
 [assembly: AssemblyCompany("Quantisle")]
 [assembly: AssemblyCopyright("Copyright © Quantisle 2010-2020. All rights reserved.")]
 
