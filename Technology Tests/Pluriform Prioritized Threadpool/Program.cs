5ddf37098255fa1551cbd0b42420a27ee79d581f
Ger Hobbelt <ger@hobbelt.com>
2021-06-10 01:21:40 +0200

    Added preliminary threadpool rework test application to the MSVC solution file in the Technology Tests directory. Also using this as an opportunity to trial those "Shared Source Project" types MSVC2019 has -- though I haven't been able to create them very easily any more in the MSVC bleeding edge release?   :-S

---
+++ Technology Tests/Pluriform Prioritized Threadpool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pluriform_Prioritized_Threadpool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Technology Tests/Technology Library/Pluriform_Prioritized_Threadpool.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Technology_Library
+{
+    class Pluriform_Prioritized_Threadpool
+    {
+    }
+}
