80eab91ad009a7f525d114612d68ad81d41851b9
Ger Hobbelt <ger@hobbelt.com>
2021-07-27 11:08:32 +0200

    tweak the MuPDF binaries fetcher: grab them from the currently active build line (Debug or Release), BUT ALSO check the Release build line anyway when we're in Debug Build mode ourselves, to ensure we've got the latest MuPDF builds that would suit us.
    This way, we automatically fetch & use Debug build MuPDF binaries when we're in Debug Build mode ourselves and the MuPDF project has been (re)built most recently in Debug Build mode as well! (32-bit mode only, for now, as there's big trouble executing 64-bit executables from 32-bit .NET binaries -- and Qiqqa itself is still 32-bit due to the lingering legacy codes. *Sigh.*

--- Qiqqa.Build/copy_mupdf_binaries.cmd
+++ Qiqqa.Build/copy_mupdf_binaries.cmd
@@ -6,12 +6,19 @@ echo "### CopyMuPDFBinaries ARGS: %1 %2 %3"
 cd "%3"
 cd ..\MuPDF\platform\win32
 
+
+IF "%2"=="Release" GOTO skipped_first_message
+echo "First we check the MuPDF %2 Build environment:"
+:skipped_first_message
+
+
 rem go with the 64-bit builds.
 
 rem cd "bin\%2-Unicode-64bit-x64"
 rem cd "bin\Release-Unicode-64bit-x64"
 rem cd "bin\%2-Unicode-32bit-x86"
-cd "bin\Release-Unicode-32bit-x86"
+
+cd "bin\%2-Unicode-32bit-x86"
 
 echo "MuPDF source directory for the binaries:"
 cd
@@ -21,5 +28,25 @@ mkdir "%1\MuPDF"
 REM update the files in the target directory:
 robocopy . "%1\MuPDF" *.exe *.dll /PURGE /XO /NP /XF mu-office-*.exe bin2coff.exe
 
+
+
+IF "%2"=="Release" GOTO skipped_second_chance
+
+
+
+echo "Next we check the MuPDF Release Build environment:"
+cd "..\..\bin\Release-Unicode-32bit-x86"
+
+echo "MuPDF source directory for the binaries:"
+cd
+
+mkdir "%1\MuPDF"
+
+REM update the files in the target directory:
+robocopy . "%1\MuPDF" *.exe *.dll /XO /NP /XF mu-office-*.exe bin2coff.exe
+
+
+
+:skip_second_chance
 
 echo "### Copying done! ################################################"
