e5219ce621700087f16334df6ab0ed23bfd17e1d
Ger Hobbelt <ger@hobbelt.com>
2022-09-16 19:44:29 +0200

    upgrading NuGet packages

--- Utilities/Misc/Constants.cs
+++ Utilities/Misc/Constants.cs
@@ -17,8 +17,8 @@ namespace Utilities
         // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
         public const string QiqqaDevProjectDir = "W:/Projects/sites/library.visyond.gov/80/lib/tooling/qiqqa/Utilities/";
         public const string QiqqaDevSolutionDir = "W:/Projects/sites/library.visyond.gov/80/lib/tooling/qiqqa/";
-        public const string QiqqaDevTargetDir = "W:/Projects/sites/library.visyond.gov/80/lib/tooling/qiqqa/Utilities/bin/Release/";
-        public const string QiqqaDevBuild = "Release";
+        public const string QiqqaDevTargetDir = "W:/Projects/sites/library.visyond.gov/80/lib/tooling/qiqqa/Utilities/bin/Debug/";
+        public const string QiqqaDevBuild = "Debug";
         // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
         // These 4 constants are live-patched by the Pre-build task (Qiqqa.Build/patch_settings_file.js)
 #endif
