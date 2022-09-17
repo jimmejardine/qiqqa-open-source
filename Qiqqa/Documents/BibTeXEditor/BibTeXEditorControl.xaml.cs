e740b153fa1f6023016638825a778949d98614a1
Ger Hobbelt <ger@hobbelt.com>
2022-09-17 13:57:59 +0200

    good_6 -- @#$%^&* WTF? TAB does it... non comprendo...

--- Qiqqa/Documents/BibTeXEditor/BibTeXEditorControl.xaml.cs
+++ Qiqqa/Documents/BibTeXEditor/BibTeXEditorControl.xaml.cs
@@ -521,7 +521,7 @@ namespace Qiqqa.Documents.BibTeXEditor
 
                         case '\t':
                             // keep
-                            //ch = ' ';
+                            ch = ' ';
                             break;
 
                         default:
