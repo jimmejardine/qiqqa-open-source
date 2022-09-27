edae3239d232a7738a17d6ce81de57e4ad921e06
Ger Hobbelt <ger@hobbelt.com>
2022-09-27 23:16:46 +0200

    tweak the git upload script to include `git fetch --all` after the push, so we have a single script/command to sync back&forth

--- git_push_all.sh
+++ git_push_all.sh
@@ -1,7 +1,21 @@
 #! /bin/bash
 #
 
+cat <<EOF
+
+>>> Pushing our work to both remotes...
+
+EOF
 for f in GerHobbelt jimmejardine ; do 
 	echo $f 
 	git push --all git@github.com:$f/qiqqa-open-source.git
 done
+
+# fetch all remote work as an afterthought
+cat <<EOF
+
+
+>>> Fetching all remote work...
+
+EOF
+git fetch --all 
