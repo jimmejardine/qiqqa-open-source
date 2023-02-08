a7e0b86eafe9bc5288496f8766c1d32811820a58
Ger Hobbelt <ger@hobbelt.com>
2023-02-08 18:28:14 +0100

    cleanup: removed several obsoleted research projects

--- Utilities/Misc/Constants.cs
+++ Utilities/Misc/Constants.cs
@@ -14,9 +14,9 @@ namespace Utilities
 
         // These 4 constants are live-patched by the Pre-build task (Qiqqa.Build/patch_settings_file.js)
         // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-        public const string QiqqaDevProjectDir = "Z:/lib/tooling/qiqqa/Utilities/";
-        public const string QiqqaDevSolutionDir = "Z:/lib/tooling/qiqqa/";
-        public const string QiqqaDevTargetDir = "Z:/lib/tooling/qiqqa/Utilities/bin/Debug/";
+        public const string QiqqaDevProjectDir = "W:/Projects/sites/library.visyond.gov/80/lib/tooling/qiqqa/Utilities/";
+        public const string QiqqaDevSolutionDir = "W:/Projects/sites/library.visyond.gov/80/lib/tooling/qiqqa/";
+        public const string QiqqaDevTargetDir = "W:/Projects/sites/library.visyond.gov/80/lib/tooling/qiqqa/Utilities/bin/Debug/";
         public const string QiqqaDevBuild = "Debug";
         // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
         // These 4 constants are live-patched by the Pre-build task (Qiqqa.Build/patch_settings_file.js)
